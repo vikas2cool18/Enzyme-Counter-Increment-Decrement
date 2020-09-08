94ede98d0429ee347cbd1425ecd56f4dd7485e1b
Vikas Bharti <Vikas_Bharti@cargill.com>
2020-09-08 16:09:08 +0530

    adding enzyme tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react';/* 
+import { render } from '@testing-library/react'; */
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdaptor from 'enzyme-adapter-react-16'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({adapter: new EnzymeAdaptor()})
+
+test('renders without error', ()=> {
+  const wrapper=shallow(<App />)
+  const appComponent=wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1);
+})
+
+test('renders increment button', ()=> {
+  
+})
+
+test('renders counter display', ()=> {
+  
+})
+
+test('counter starts at zero', ()=> {
+  
+})
+
+test('clicking button implements the counter display', ()=> {
+  
+})
